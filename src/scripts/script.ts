1ec7f174243fd88c79432aea4b542bfb19bf2033
KAREEM AGHA <>
2024-12-13 17:47:16 +1100

    Completed extension

--- src/scripts/script.ts
+++ src/scripts/script.ts
@@ -1,16 +1,60 @@
-const read_content = function() {
+require('dotenv').config()
+
+const read_content = async function() {
     const questions = document.getElementsByClassName("filter_mathjaxloader_equation")
     console.log(questions.length)
+    let image_counter = 0
     let question_text = ""
-    // Loop through question boxes
     for (let i = 0; i < questions.length; i++) {
-        // Loop through <p> in question box
         for (let j = 0; j < questions[i].getElementsByTagName("p").length; j++) {
-            let depth = 0;
             const words = questions[i].getElementsByTagName("p")[j]
-            question_text = recursive_search(question_text, words)
-
-
+            for(let g = 0; g < words.childNodes.length; g++) {
+                let child = words.childNodes[g]
+                if (child.nodeType === Node.TEXT_NODE) {
+                    question_text += child.nodeValue
+                    // console.log(words.childNodes[g].nodeValue)
+                } else if (child.nodeType === Node.ELEMENT_NODE) {
+                    let flag = 0
+                    if(child.childNodes.length > 0) {
+                        for (let k = 0; k < child.childNodes.length; k++) {
+                            if (child.childNodes[k].nodeName === "STRONG") {
+                                question_text = question_text + "\\textbf{" + child.textContent + "}"
+                                flag = 1
+                            }
+                            
+                            if (child.childNodes[0].nodeName === "IMG") {
+                                const img_element = child.childNodes[0] as HTMLImageElement;
+                                const image_src = img_element.src;
+                                let file_name = `${Date.now()}_${image_counter}`
+                                downlaod_image(image_src, file_name);
+                                question_text = question_text + `\\begin{center}\n\\includegraphics[width=0.8\\textwidth]{images/${file_name}.png}\n\\end{center}`
+                                image_counter++;
+                                flag = 1
+                            }
+                        }
+                    }
+                    if (flag === 0) {
+                        if (child.nodeName === "STRONG") {
+                            question_text = question_text + "\\textbf{" + child.textContent + "}"
+                        } else if (child.nodeName === "IMG") {
+                            const img_element = child as HTMLImageElement;
+                            const image_src = img_element.src;
+                            // const imageUrl = await upload_image(image_src);
+                            // question_text = question_text + `\\begin{center}\n\\includegraphics[width=0.8\\textwidth]{${imageUrl}}\n\\end{center}`
+                            let file_name = `${Date.now()}_${image_counter}`
+                            downlaod_image(image_src, file_name);
+                            question_text = question_text + `\\begin{center}\n\\includegraphics[width=0.8\\textwidth]{images/${file_name}.png}\n\\end{center}`
+                            image_counter++;
+                        } else if (child.nodeName === "INPUT") {
+                            question_text = question_text + " \\underline{\\hspace{3cm}} "
+                        } else {
+                            question_text = question_text + "$" + String(child.textContent) + "$"
+                        }
+                        console.log(child.nodeName)
+                        // console.log(words.childNodes[g].textContent)
+                    }
+                }
+            }
             // Adding 2 new line chars + latex new line char \\
             question_text = question_text + '\\\\\n\n'
         }
@@ -18,27 +62,69 @@ const read_content = function() {
     navigator.clipboard.writeText(question_text)
 } 
 
-const recursive_search = function(question_text: string, words: any): string {
-    for (let g = 0; g < words.childNodes.length; g++) {
-        let child = words.childNodes[g]
-        if (child.nodeType === Node.TEXT_NODE) {
-            question_text += child.nodeValue
-        } else if (child.nodeType === Node.ELEMENT_NODE) {
-            if (child.childNodes.length > 1) {
-                question_text = recursive_search(question_text, words)
-            } else {
-                if (child.nodeName === "STRONG") {
-                    question_text = question_text + "\\textbf{" + child.textContent + "}"
-                } else if (child.nodeName === "IMG") {
-                    // Todo: Upload image to imgur
-                } else {
-                    question_text = question_text + "\\(" + String(child.textContent) + "\\)"
-                }
-            }
+
+const upload_image = async function(image_src: string): Promise<string> {
+    try {
+        const res = await fetch(image_src);
+        const myBlob = await res.blob();
+        
+        const image_file = new File([myBlob], 'image.jpeg', {type: myBlob.type});
+        const formData = new FormData();
+        formData.append('image', image_file);
+        formData.append('album', `${process.env.DELETE_HASH}`);
+        
+        const response = await fetch('https://api.imgur.com/3/image', {
+            method: 'POST',
+            headers: new Headers({
+                Authorization: `Client-ID ${process.env.CLIENT_ID}`
+            }),
+            body: formData
+        });
+        
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
         }
+        
+        const data = await response.json();
+        const imageUrl = data.data.link;
+        console.log('Image uploaded to album! Link: ' + imageUrl);
+        return imageUrl;
+    } catch (error) {
+        console.error(JSON.stringify(error));
+        console.log('Upload failed: ');
+        throw error;
     }
-    return question_text
 }
+
+
+const downlaod_image = function(url: string, filename: string) {
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', url, true);
+  xhr.responseType = 'blob';
+
+  xhr.onload = function() {
+    if (this.status === 200) {
+      const blob = this.response;
+      const a = document.createElement('a');
+      a.href = URL.createObjectURL(blob);
+      a.download = filename;
+      a.style.display = 'none';
+      document.body.appendChild(a);
+      a.click();
+      document.body.removeChild(a);
+      URL.revokeObjectURL(a.href);
+    } else {
+      console.error('Failed to download image:', url);
+    }
+  };
+
+  xhr.onerror = function() {
+    console.error('Error downloading image:', url);
+  };
+
+  xhr.send();
+}
+
 
 console.log("Script loaded");
 window.onload = () => {
